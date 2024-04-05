7876b2f32f08a9c6b2fa5bb85d4556c3546c5860
Dhan Lomander <Dhan.Lomander@hedinit.com>
2024-04-05 21:34:25 +0200

    Created the needed projects and structured and organised folders

---
+++ GotheTollway.Database/Context/GotheTollContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace GotheToll.Database.Context
+{
+    public class GotheTollContext : DbContext
+    {
+        protected GotheTollContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
